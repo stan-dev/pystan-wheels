5f8c9040ce392215fb411a04c7d5db6131d47085
Allen Riddell <riddella+stan@indiana.edu>
2020-07-05 20:17:20 -0400

    fix: Remove custom gcc

--- env_vars.sh
+++ env_vars.sh
@@ -1,5 +1,3 @@
 # See multibuild/README.rst
 export MACOSX_DEPLOYMENT_TARGET=10.9
 export CFLAGS="-static-libstdc++"
-export CC=/usr/local/gcc-8.3.0/bin/gcc-8.3.0
-export CXX=/usr/local/gcc-8.3.0/bin/g++-8.3.0
